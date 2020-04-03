eba3b2d2f24497b67cfb0ca068fceae1b4b6b3f4
Amy Holt <amy.elizabeth.holt@gmail.com>
2020-04-03 14:53:54 -0600

    Remove passport for now

--- public/js/passport.js
+++ public/js/passport.js
@@ -1,82 +1,82 @@
-let relativeURL = window.location.pathname;
-
-document.addEventListener("DOMContentLoaded", function() {
-  const logOutForm = document.querySelector('.logout-form');
-  const logInForm = document.querySelector('.login-form');
-
-  function checkLocation() {
-    const check = localStorage.getItem('loggedIn') || 'false';
-
-    if (relativeURL === '/kwk-curriculum-site/login/' && check === 'true') {
-      showLinks();
-      displayLogout();
-    } else if (relativeURL === '/kwk-curriculum-site/login/' && check === 'false') {
-      displayLogin();
-      var loginBtn = document.querySelector("#login-submit");
-      loginBtn.addEventListener('click', logIn);
-    } else if (relativeURL !== '/kwk-curriculum-site/login/' && check === 'true') {
-      showLinks();
-    } else if (relativeURL !== '/kwk-curriculum-site/login/' && check === 'false') {
-      window.location.replace('/kwk-curriculum-site/login/');
-    }
-
-  }
-
-  checkLocation();
-
-  function displayLogin() {
-    logOutForm.style.display = 'none';
-    logInForm.style.display = 'block';
-  }
-
-  function displayLogout() {
-    logOutForm.style.display = 'block';
-    logInForm.style.display = 'none';
-    logOutForm.addEventListener('submit', logOut);
-  }
-
-  function showLinks() {
-    var links = document.querySelectorAll(".navbar-navigation--links .curriculum-links");
-    for (var i = 0; i < 2; i++) {
-      links[i].style.display = 'block';
-    }
-  }
-
-  function logIn(event) {
-    event.preventDefault();
-
-    let login = document.getElementById("login").value.trim();
-    let password = document.getElementById("password").value.trim();
-
-    if (checkLogin(login) && checkPassword(password)) {
-      localStorage.setItem('loggedIn', 'true');
-      window.location.replace("/kwk-curriculum-site/");
-      showLinks();
-    } else {
-      alert("Sorry! Try again.")
-    }
-  }
-
-  function hideLinks() {
-    var links = document.querySelectorAll(".navbar-navigation--links .curriculum-links");
-    for (var i = 0; i < 2; i++) {
-      links[i].style.display = 'none';
-    }
-  }
-
-  function checkLogin(login) {
-    return login === "kwk";
-  }
-
-  function checkPassword(password) {
-    return password === "boss";
-  }
-
-  function logOut(event) {
-    event.preventDefault();
-    localStorage.setItem('loggedIn', 'false');
-    window.location.replace("/kwk-curriculum-site/login/");
-    hideLinks();
-  }
-
-});
+// let relativeURL = window.location.pathname;
+//
+// document.addEventListener("DOMContentLoaded", function() {
+//   const logOutForm = document.querySelector('.logout-form');
+//   const logInForm = document.querySelector('.login-form');
+//
+//   function checkLocation() {
+//     const check = localStorage.getItem('loggedIn') || 'false';
+//
+//     if (relativeURL === '/kwk-curriculum-site/login/' && check === 'true') {
+//       showLinks();
+//       displayLogout();
+//     } else if (relativeURL === '/kwk-curriculum-site/login/' && check === 'false') {
+//       displayLogin();
+//       var loginBtn = document.querySelector("#login-submit");
+//       loginBtn.addEventListener('click', logIn);
+//     } else if (relativeURL !== '/kwk-curriculum-site/login/' && check === 'true') {
+//       showLinks();
+//     } else if (relativeURL !== '/kwk-curriculum-site/login/' && check === 'false') {
+//       window.location.replace('/kwk-curriculum-site/login/');
+//     }
+//
+//   }
+//
+//   checkLocation();
+//
+//   function displayLogin() {
+//     logOutForm.style.display = 'none';
+//     logInForm.style.display = 'block';
+//   }
+//
+//   function displayLogout() {
+//     logOutForm.style.display = 'block';
+//     logInForm.style.display = 'none';
+//     logOutForm.addEventListener('submit', logOut);
+//   }
+//
+//   function showLinks() {
+//     var links = document.querySelectorAll(".navbar-navigation--links .curriculum-links");
+//     for (var i = 0; i < 2; i++) {
+//       links[i].style.display = 'block';
+//     }
+//   }
+//
+//   function logIn(event) {
+//     event.preventDefault();
+//
+//     let login = document.getElementById("login").value.trim();
+//     let password = document.getElementById("password").value.trim();
+//
+//     if (checkLogin(login) && checkPassword(password)) {
+//       localStorage.setItem('loggedIn', 'true');
+//       window.location.replace("/kwk-curriculum-site/");
+//       showLinks();
+//     } else {
+//       alert("Sorry! Try again.")
+//     }
+//   }
+//
+//   function hideLinks() {
+//     var links = document.querySelectorAll(".navbar-navigation--links .curriculum-links");
+//     for (var i = 0; i < 2; i++) {
+//       links[i].style.display = 'none';
+//     }
+//   }
+//
+//   function checkLogin(login) {
+//     return login === "kwk";
+//   }
+//
+//   function checkPassword(password) {
+//     return password === "boss";
+//   }
+//
+//   function logOut(event) {
+//     event.preventDefault();
+//     localStorage.setItem('loggedIn', 'false');
+//     window.location.replace("/kwk-curriculum-site/login/");
+//     hideLinks();
+//   }
+//
+// });
